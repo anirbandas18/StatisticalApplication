326615325a9fb52e1d27f4eeea92a50aac3c1614
Anirban Das <anirbandas18@live.com>
2017-06-02 16:14:00 +0530

    Added error response from controller

--- StatisticalApplication/src/main/java/com/sss/bridge/controller/NormalDistributionController.java
+++ StatisticalApplication/src/main/java/com/sss/bridge/controller/NormalDistributionController.java
@@ -20,71 +20,50 @@ import com.sss.bridge.service.NormalDistributionService;
 @WebServlet("/NormalDistributionController")
 public class NormalDistributionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NormalDistributionController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public NormalDistributionController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		doPost(request, response);
 	}
 
 	/**
-	 * @param INTEGER 
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @param INTEGER
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		NormalDistribution d=new NormalDistribution();
-
-String x = request.getParameter("samples");
-String m= request.getParameter("mean");
-String v=request.getParameter("variance");
-d.setSamples(Integer.parseInt(x));
-d.setMean(Double.parseDouble(m));
-d.setVariance(Double.parseDouble(v));
-System.out.println(d);
-System.out.println("Successful..");
-/*PrintWriter out =response.getWriter();
-//out.println(d);\
-String s="F:/output(2).txt";
-//ServletContext content=getServletContext();
-InputStream is = new FileInputStream(s);
-System.out.println(is);
-//out.print(d);
-if(is!=null)
-{
-	InputStreamReader i=new InputStreamReader(is);
-	BufferedReader reader = new BufferedReader(i);
-	//PrintWriter writer = response.getWriter();
-	String text = "";
-	text = reader.readLine(); 
-		System.out.println(text);
-		out.print(text);
+		NormalDistribution d = new NormalDistribution();
+		String x = request.getParameter("samples");
+		String m = request.getParameter("mean");
+		String v = request.getParameter("variance");
+		d.setSamples(Integer.parseInt(x));
+		d.setMean(Double.parseDouble(m));
+		d.setVariance(Double.parseDouble(v));
+		System.out.println(d);
+		System.out.println("Successful..");
+		try {
+			NormalDistributionService service = new NormalDistributionService("F:/script/sample.r");
+			String graphInBytes = service.plotScatter(d);
+			PrintWriter out = response.getWriter();
+			out.println(graphInBytes);
+		} catch (NormalDistributionException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+		}
+	}
 }
-out.flush();
-	*/
-//NormalDistributionService service = new NormalDistributionService("F:/script/sample.r");
-//String graphInBytes;
-try {
-	NormalDistributionService service = new NormalDistributionService("F:/script/sample.r");
-	String graphInBytes;
-	graphInBytes = service.plotScatter(d);
-	PrintWriter out =response.getWriter();
-	out.println(graphInBytes);
-} catch (NormalDistributionException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-//PrintWriter out =response.getWriter();
-//out.println(graphInBytes);
-}}
